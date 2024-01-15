049e3457297eca4c256778d377ca1203ce11cec0
MuazAsif-Dev <muazasif.info@gmail.com>
2024-01-15 16:24:14 -0500

    feat: add react query provider to list of providers

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
@@ -11,10 +12,14 @@ export function Providers({
   children: React.ReactNode;
   session: any;
 }) {
+  const queryClient = new QueryClient();
+
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" disableTransitionOnChange>
+      <QueryClientProvider client={queryClient}>
         <SessionProvider session={session}>{children}</SessionProvider>
+      </QueryClientProvider>
       </NextThemesProvider>
     </NextUIProvider>
   );
