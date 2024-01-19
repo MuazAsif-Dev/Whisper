6965d7eb1810799889baa1b39173d8994da80ea2
MuazAsif-Dev <muazasif.info@gmail.com>
2024-01-19 09:48:31 -0500

    fix: drizzle-kit migration file bug, only generates one foreign key action at a time

---
+++ server/src/db/migrations/0006_steep_the_hand.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "room_members" DROP CONSTRAINT "room_members_user_id_users_id_fk";
+
+DO $$ BEGIN
+ ALTER TABLE "room_members" ADD CONSTRAINT "room_members_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
